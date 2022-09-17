c80ae659b82c83b26e1fd855173de161e3d4fde8
Emily Castillo <emilycastillox@gmail.com>
2022-09-16 20:32:23 -0400

    added sign up component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/login/LoginForm';
+import SignUpForm from './components/signup/SignUpForm';
 
 
 function App() {
-  const test = 'candy';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <LoginForm />
+    <SignUpForm />
+    </>
   );
 }
 
